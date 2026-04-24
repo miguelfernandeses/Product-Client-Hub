a430c2c22c009c62d9ac701d4582c6f68c905c32
Miguel Fernandes <miguelfernandeses@gmail.com>
2026-04-24 18:06:43 -0300

    Criando endpoints GET, POST, PUT e DELETE
    Criação do controller espercífico para clientes

---
+++ ProductClientHub/Controllers/ClientsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClientHub.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}/miguel{sobrenome}")]
+        public IActionResult GetById(Guid id, string sobrenome)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}

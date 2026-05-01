dc8643cf24e53a5efd252463ab039c86e72d83f1
Miguel Fernandes <miguelfernandeses@gmail.com>
2026-05-01 13:16:50 -0300

    Configurando o Entity Framework no projeto
    Configurando o ORM Entity Framework onde ele faz a tradução de objetos em queries facilitando a manipulação de dados no banco de dados. Instalações de bibliotecas necessárias e criação da classe DbContext para gerenciar tabelas de clientes e produtos

---
+++ ProductClientHub/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; } = default!;
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source= SEU-CAMINHO");
+        }
+    }
+}

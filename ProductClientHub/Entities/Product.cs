f13b6d33319b2d40c81932cf352756863248a53b
Miguel Fernandes <miguelfernandeses@gmail.com>
2026-05-06 15:34:24 -0300

    Adicionando produtos na API
    Corrigindo um erro sobre o endpoint de atualização de clientes ajustando os códigos de resposta para incluir 0 400 BadRequest. Criação de um novo endpoint para registrar produtos associados a clientes. Validando informações recebidas e criando regras para garantir que os dados dos produto sejam válidos. Também feita a herança para simplificar as entidades

--- ProductClientHub/Entities/Product.cs
+++ ProductClientHub/Entities/Product.cs
@@ -1,13 +1,12 @@
 ﻿namespace ProductClientHub.API.Entities
 {
-    public class Product
+    public class Product : EntityBase
     {
-        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Brand { get; set; } = string.Empty;
         public decimal Price { get; set; } 
 
-        public Guid CLientId { get; set; }
+        public Guid ClientId { get; set; }
 
 
     }

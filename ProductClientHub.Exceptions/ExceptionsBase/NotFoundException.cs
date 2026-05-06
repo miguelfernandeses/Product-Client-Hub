d70502e0573e25d065cd02ac506aecd3bfb9baf4
Miguel Fernandes <miguelfernandeses@gmail.com>
2026-05-06 14:13:58 -0300

    Editando informações de um cliente
    Implementando a funcionalidade de edição de dados de um cliente, essencial para corrigir informações. Usando o ID do cliente para garantir que altere o registro certo. Tratando exceções personalizada e ententendo como lidar com dados opcionais durante a edição

---
+++ ProductClientHub.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase
+{
+    public class NotFoundException : ProductClientHubExceptions
+    {
+        public NotFoundException(string errorMessage) : base(errorMessage)
+        {
+
+        }
+
+        public override List<string> GetErrors() => [Message];
+
+        public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.NotFound;
+    }
+}
---
+++ ProductClientHub/UseCases/Clients/Update/UpdateClientUseCase.cs
@@ -0,0 +1,44 @@
+﻿using ProductClientHub.API.Infrastructure;
+using ProductClientHub.API.UseCases.Clients.SharedValidator;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.API.UseCases.Clients.Update;
+
+public class UpdateClientUseCase
+{
+    public void Execute(Guid clientId, RequestClientJson request)
+    {
+        Validate(request);
+
+        var dbContext = new ProductClientHubDbContext();
+
+        var entity = dbContext.Clients.FirstOrDefault(client => client.Id == clientId);
+        if (entity is null)
+            throw new NotFoundException("Cliente não encontrado.");
+
+        entity.Name = request.Name;
+        entity.Email = request.Email;
+
+        dbContext.Clients.Update(entity);
+        dbContext.SaveChanges();
+
+
+    }
+
+
+    private void Validate(RequestClientJson request)
+    {
+        var validator = new RequestClientValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errors = result.Errors.Select(failure => failure.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errors);
+
+        }
+    }
+}

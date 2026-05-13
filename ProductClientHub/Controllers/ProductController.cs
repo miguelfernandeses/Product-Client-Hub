41fcf5fd61693be866620572fca506599a858b30
Miguel Fernandes <miguelfernandeses@gmail.com>
2026-05-13 14:30:26 -0300

    Excluindo clientes e produtos
    Explorando a exclusão de produtos e clientes no banco de dados. Implementando um endpoint para deletar produto onde verifica  se o produto existe antes de removê-lo. Em seguida apliquei o mesmo conceito para clientes.

--- ProductClientHub/Controllers/ProductController.cs
+++ ProductClientHub/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductClientHub.API.UseCases.Clients.Register;
+using ProductClientHub.API.UseCases.Products.Delete;
 using ProductClientHub.Communication.Requests;
 using ProductClientHub.Communication.Responses;
 
@@ -21,6 +22,20 @@ namespace ProductClientHub.API.Controllers
             var response = useCase.Execute(clientId, request);
 
             return Created(string.Empty, response);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status404NotFound)]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var useCase = new DeleteProductUseCase();
+
+             useCase.Execute(id);
+
+             return NoContent();
+           
         }
     }
 }
